89f84defcb9aef1834335f25c90f64bc3bd32c7f
Victor <victorgt2406@github.com>
2023-05-20 22:07:08 +0200

    messages routes schema

---
+++ routes/messages.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const router = express.Router();
+// create a message
+router.post('/',);
+
+// get user messages
+router.get('/',);
+
+// get a message
+router.get('/:id',);
+
+// delete a message
+router.delete('/:id',);
+
+// module.exports = router;
+export default router;
