95a7b2a25eb9b996e1ca904479a225826719253a
Victor <victorgt2406@github.com>
2023-05-23 18:47:14 +0200

    sportsModel

--- models/index.ts
+++ models/index.ts
@@ -1,5 +1,6 @@
 import messages from "./messages";
 import teams from "./teams";
 import users from "./users";
+import sports from "./sports";
 
-export { users as usersModel, messages as messagesModel, teams as teamsModel };
+export { users as usersModel, messages as messagesModel, teams as teamsModel, sports as sportsModel };
